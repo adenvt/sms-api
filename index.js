f3c792243a95af6ab12eed5a3dc0b791e74d6923
Ade Novid <adenvt@gmail.com>
2017-11-14 11:34:52 +0700

    add key & some comment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mqtt = require('mqtt')
 const bodyParser = require('body-parser')
+
 const app = express()
 const client = mqtt.connect('tcp://iot.eclipse.org:1883')
+const key = '123456789'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,6 +17,7 @@ app.post('/sms', (req, res) => {
   let no = req.body.no
   let message = req.body.message
 
+  // If No or Message Empty
   if (!no || !message) {
     res.json({
       success: false,
@@ -23,13 +26,20 @@ app.post('/sms', (req, res) => {
     return
   }
 
-  client.publish('sendSMS', JSON.stringify({ no, message }))
-  res.json({success: true, message: 'OK'})
+  // Send Message to Phone
+  client.publish('sendSMS', JSON.stringify({ no, message, key }))
+
+  // Response Output
+  res.json({
+    success: true,
+    message: 'OK'
+  })
 })
 
 app.listen(8080, () => {
   console.log('Listening on port 8080!')
-  client.on('connect', () => {
-    console.log('Connected to MTQQ Server!')
-  })
+})
+
+client.on('connect', () => {
+  console.log('Connected to MTQQ Server!')
 })
